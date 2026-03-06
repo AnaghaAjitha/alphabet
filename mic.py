a825fb1d5ae55bd80e8bbce63c310a5b195390b9
AnaghaAjitha <168955434+AnaghaAjitha@users.noreply.github.com>
2026-03-06 12:05:30 +0530

    Update mic.py

--- mic.py
+++ mic.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import numpy as np
@@ -6,12 +5,15 @@ import librosa
 import sounddevice as sd
 
 
+# ===============================
+# PARAMETERS (same as training)
+# ===============================
 SAMPLE_RATE = 16000
 DURATION = 1
 TARGET_LENGTH = SAMPLE_RATE * DURATION
 
-N_MELS = 64
-N_FFT = 512
+N_MELS = 96
+N_FFT = 1024
 HOP_LENGTH = 160
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -21,58 +23,72 @@ alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
 idx_to_label = {i: alphabet[i] for i in range(26)}
 
 
-# MODEL(same as training) 
-class BalancedCNN_BiLSTM(nn.Module):
+# ===============================
+# ATTENTION MODEL
+# ===============================
+class AttentionModel(nn.Module):
     def __init__(self, num_classes=26):
-        super(BalancedCNN_BiLSTM, self).__init__()
+        super().__init__()
 
         self.cnn = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.Conv2d(1, 32, 3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
+            nn.MaxPool2d((2,2)),
 
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.Conv2d(32, 64, 3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
+            nn.MaxPool2d((2,2)),
 
-            nn.MaxPool2d((2, 1))
+            nn.MaxPool2d((2,1))
         )
 
         self.lstm = nn.LSTM(
             input_size=64 * (N_MELS // 8),
-            hidden_size=128,
+            hidden_size=96,
             num_layers=1,
             batch_first=True,
             bidirectional=True
         )
 
+        self.attention = nn.Linear(96 * 2, 1)
+
         self.dropout = nn.Dropout(0.5)
-        self.fc = nn.Linear(256, num_classes)
+        self.fc = nn.Linear(96 * 2, num_classes)
 
     def forward(self, x):
+
         x = x.unsqueeze(1)
         x = self.cnn(x)
 
-        b, c, m, t = x.size()
-        x = x.permute(0, 3, 1, 2)
-        x = x.reshape(b, t, c * m)
+        batch, channels, mel, time = x.size()
+
+        x = x.permute(0,3,1,2)
+        x = x.reshape(batch, time, channels * mel)
 
         x, _ = self.lstm(x)
-        x = x[:, -1, :]
+
+        attn_weights = torch.softmax(self.attention(x), dim=1)
+        x = torch.sum(attn_weights * x, dim=1)
+
         x = self.dropout(x)
         x = self.fc(x)
 
         return x
 
-model = BalancedCNN_BiLSTM().to(DEVICE)
-model.load_state_dict(torch.load("final_best_model.pth", map_location=DEVICE))
+
+# ===============================
+# LOAD MODEL
+# ===============================
+model = AttentionModel(num_classes=26).to(DEVICE)
+model.load_state_dict(torch.load("attention_best_model_5.pth", map_location=DEVICE))
 model.eval()
 
 print("Model loaded successfully!")
-print("Press ENTER")
+print("Press ENTER to record")
 print("Type 'q' then ENTER to quit.\n")
+
 
 # ===============================
 # PREPROCESS FUNCTION
@@ -103,29 +119,39 @@ def preprocess_audio(audio):
     return mel_db
 
 
+# ===============================
+# MIC LOOP
+# ===============================
 while True:
+
     user_input = input("Press ENTER to record: ")
 
     if user_input.lower() == "q":
         print("Exiting.")
         break
 
-    print("🎙 Recording...")
-    audio = sd.rec(int(SAMPLE_RATE * DURATION),
-                   samplerate=SAMPLE_RATE,
-                   channels=1,
-                   dtype='float32')
+    print("Recording... Speak now")
+
+    audio = sd.rec(
+        int(SAMPLE_RATE * DURATION),
+        samplerate=SAMPLE_RATE,
+        channels=1,
+        dtype="float32"
+    )
+
     sd.wait()
 
     audio = audio.flatten()
     input_tensor = preprocess_audio(audio)
 
     with torch.no_grad():
+
         output = model(input_tensor)
         probs = torch.softmax(output, dim=1)
+
         confidence, pred = torch.max(probs, 1)
 
     predicted_letter = idx_to_label[pred.item()]
     conf_percent = confidence.item() * 100
 
-    print(f"Predicted: {predicted_letter.upper()}  |  Confidence: {conf_percent:.2f}%\n")
+    print(f"Predicted: {predicted_letter.upper()} | Confidence: {conf_percent:.2f}%\n")
